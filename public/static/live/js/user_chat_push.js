99e319a272ec48aa5db30fb4a6c17d8382bd2cd3
QvBiLam <qvbilam@163.com>
2020-01-14 22:34:16 +0800

    up

--- public/static/live/js/user_chat_push.js
+++ public/static/live/js/user_chat_push.js
@@ -1,13 +1,13 @@
 $(function () {
 	var $submitBtn = $('#submit-btn');
 	//获取当前的请求地址
-	var host = window.location.host;
-	// var host = 'http://live.qvbilam.xin:9503/';
+	// var host = window.location.host;
+	var host = 'http://live.qvbilam.xin:9503/';
 	//获取当前协议
 	var agreement = window.location.protocol;
 	// var send_url = agreement + '//' + host + '/index/chat/index'
 	var send_url = host + '/index/chat/index'
-    console.log($.cookie(host))
+    console.log($.cookie('token'))
     console.log(12341231)
 	$('#chatPush').keydown(function (event) {
 
@@ -15,8 +15,8 @@ $(function () {
 		if (event.keyCode == 13) {
 			var text = $(this).val();
 			$(this).val('')
-            if($.cookie(host)){
-                userInfo =  $.cookie(host)
+            if($.cookie('token')){
+                userInfo =  $.cookie('token')
             }{
                 userInfo = ''
             }
@@ -32,7 +32,7 @@ $(function () {
 				url: send_url,
 				data: data,
 				beforeSend: function (XMLHttpRequest) {
-					XMLHttpRequest.setRequestHeader("authorization", $.cookie(host));
+					XMLHttpRequest.setRequestHeader("authorization", $.cookie('token'));
 				}
 			});
 		}
