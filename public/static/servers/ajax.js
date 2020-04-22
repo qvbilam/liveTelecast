f496adb40b0aa3f834513007350f4880d60765e4
zhaocaixia <342892988@qq.com>
2020-04-22 16:44:36 +0800

    'up'

--- public/static/servers/ajax.js
+++ public/static/servers/ajax.js
@@ -28,58 +28,6 @@
 // }
 
 
-
-// export function post(url, data, dataType) {
-//   return new Promise(function (open, err) {
-//     //1. 创建xhr对象
-//     var xhr = new XMLHttpRequest();
-//     //2.绑定监听事件
-//     xhr.onreadystatechange = function () {
-//       if (xhr.readyState == 4 && xhr.status == 200) {
-//         var res = xhr.responseText;
-//         if (dataType === "json")
-//           res = JSON.parse(res)
-//         open(res);
-//       }
-//     }
-//     //3.打开连接
-//     xhr.open("POST", url, true);
-//     //设置请求消息头
-//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//     //4.发送请求
-//     if (data !== undefined)
-//       xhr.send(data);
-//     else  xhr.send(null);
-//   })
-// }
-
-
-// export function get(url, data, dataType) {
-//   return new Promise(function (open, err) {
-//     //1. 创建xhr对象
-//     var xhr = new XMLHttpRequest();
-//     //2.绑定监听事件
-//     xhr.onreadystatechange = function () {
-//       console.log(xhr,'xhr')
-//       if (xhr.readyState == 4 && xhr.status == 200) {
-//         var res = xhr.responseText;
-//         if (dataType === "json")
-//           res = JSON.parse(res)
-//         open(res);
-//       }
-//     }
-//     //3.打开连接
-//     xhr.open('GET', url, true);
-//     console.log(data,'xhr.open')
-//     if (data != undefined) {
-//       url += "?" + data;
-//     }
-//     else  xhr.send(null);
-//   })
-// }
-
-
-
 /**
  * 封装简单的ajax 函数
  * @param url  请求地址
