a1177990cdee138b87ef29fa61c67bef6ddf9db9
zhaocaixia <342892988@qq.com>
2020-04-23 16:53:13 +0800

    'up'

--- public/static/live/js/user_chat_push.js
+++ public/static/live/js/user_chat_push.js
@@ -1,32 +1,35 @@
-import { chat } from './../../servers/api.js'
+// import { chat } from './../../servers/api.js'
 $(function () {
 	var $submitBtn = $('#submit-btn');
 	var token = $.cookie('token')
 	console.log(typeof token)
 	$('#chatPush').keydown(function (event) {
-
+		var host = window.location.host;
+		//获取当前协议
+		var agreement = window.location.protocol;
+		var send_url = agreement + '//' + host + '/index/chat/index'
 		/*回车事件*/
 		if (event.keyCode == 13) {
 			var text = $(this).val();
 			$(this).val('')
-			var data = 'content=' + text + '&game_id=' + 1 + "&token=" + token ? token : '123456'
+			var data = { 'content': text, 'game_id': 1, "token": token ? token : '123456' }
 			/*向服务端发送数据*/
 			// $.post(send_url, data, function (result) {
 			//     /**/
 			//
 			// }, 'json')
 
-			chat(data, function (res) {
-				console.log(res)
-			})
-			// $.ajax({
-			// 	type: "post",
-			// 	url: send_url,
-			// 	data: data,
-			// 	beforeSend: function (XMLHttpRequest) {
-			// 		XMLHttpRequest.setRequestHeader("authorization", $.cookie('token'));
-			// 	}
-			// });
+			// chat(data,function(res){
+			// 	console.log(res)
+			// })
+			$.ajax({
+				type: "post",
+				url: send_url,
+				data: data,
+				beforeSend: function (XMLHttpRequest) {
+					XMLHttpRequest.setRequestHeader("authorization", $.cookie('token'));
+				}
+			});
 		}
 
 
--- public/static/servers/api.js
+++ public/static/servers/api.js
@@ -9,7 +9,7 @@ export const sendCode = (data, callBack) => ajax(baseUrl + '/index/send/index', 
 //live/js/VerToken
 export const send = (callBack) => ajax(baseUrl + '/index/send/index', 'POST', true, null, callBack, 'json')
 //bank/js/user_chat_push 
-export const chat = (data, callBack) => ajax(baseUrl + '/index/chat/index?'+data, 'GET', true, null, callBack)
+export const chat = (data, callBack) => ajax(baseUrl + '/index/chat/index', 'POST', true, data, callBack)
 //live/detail
 export const game = (callBack) => ajax(baseUrl + '/index/game/getdata', 'POST', true, null, callBack)
 //live/perfect
