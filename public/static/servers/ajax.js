8b5b6e82e7a2ed0e225c2fe506691130569d4111
zhaocaixia <342892988@qq.com>
2020-04-26 14:35:13 +0800

    'up'

--- public/static/servers/ajax.js
+++ public/static/servers/ajax.js
@@ -7,79 +7,86 @@
  * @param Callback  回调函数(数据,对象)
  * @param type  回调数据类型 text||xml
  */
-export function ajax(url, method, async, data, callBack, type) {
-  //设置参数默认值
-  method = method || "GET";
-  method = method.toUpperCase();
-  async = async || true;
-  data = data || null;
-  callBack = callBack || function () {
-      console.log("默认回调函数");
-  };
-  type = type || "text";
-  type = type.toLowerCase();
-
-  var xhr = false;
-  //初始化XMLHttpRequest 对象
-  if (window.XMLHttpRequest) {//Mozilla 浏览器
-      xhr = new XMLHttpRequest();
-      if (xhr.overrideMimeType) {//设置MiME 类别
-          if (type == "text") {
-              xhr.overrideMimeType("text/plain");
-          } else if (type == "xml") {
-              xhr.overrideMimeType("text/xml");
-          }
-      }
-  } else if (window.ActiveXObject) { //IE 浏览器
-      try {
-          xhr = new ActiveXObject("Msxml2.XMLHTTP");
-      } catch (e) {
-          try {
-              xhr = new ActiveXObject("Microsoft.XMLHTTP");
-          } catch (e) {
-          }
-      }
-  }
-
-  //异常，创建对象实例失败
-  if (!xhr) {
-      console.log("不能创建XMLHttpRequest对象实例.");
-      return false;
-  }
-
-  xhr.open(method, url, async); //发起请求
-  xhr.setRequestHeader("If-Modified-Since", "0"); //每次都是获取最新的内容
-  if (method == "POST") { //post
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xhr.send(data);
-  } else {
-      xhr.send(null); //get 不能发送数据
-  }
-
-  //指定响应处理函数
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          //成功之后调用回调函数
-          if (type == "xml") {
-            //   return callBack(JSON.parse(xhr.responseXML), xhr);
-            if (!xhr.responseXML || typeof xhr.responseXML != "string") {
-                return callBack(xhr.responseXML, xhr);
+export function ajax(url, method, async, data, callBack, istoken, type) {
+    //设置参数默认值
+    method = method || "GET";
+    method = method.toUpperCase();
+    async = async || true;
+    data = data || null;
+    callBack = callBack || function () {
+        console.log("默认回调函数");
+    };
+    type = type || "text";
+    type = type.toLowerCase();
+    istoken = istoken || true
+    var xhr = false;
+    //初始化XMLHttpRequest 对象
+    if (window.XMLHttpRequest) {//Mozilla 浏览器
+        xhr = new XMLHttpRequest();
+        if (xhr.overrideMimeType) {//设置MiME 类别
+            if (type == "text") {
+                xhr.overrideMimeType("text/plain");
+            } else if (type == "xml") {
+                xhr.overrideMimeType("text/xml");
             }
-            if (typeof responseXML == "string") {
-                return callBack(JSON.parse(xhr.responseXML), xhr);
+        }
+    } else if (window.ActiveXObject) { //IE 浏览器
+        try {
+            xhr = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
             }
-             
-          } else if (type == "text") {
-            if (!xhr.responseText || typeof xhr.responseText != "string") {
-                return callBack(xhr.responseText, xhr);
+        }
+    }
+
+    //异常，创建对象实例失败
+    if (!xhr) {
+        console.log("不能创建XMLHttpRequest对象实例.");
+        return false;
+    }
+
+    xhr.open(method, url, async); //发起请求
+    xhr.setRequestHeader("If-Modified-Since", "0"); //每次都是获取最新的内容
+    if (istoken) {
+        var token = localStorage.getItem('token');
+        if(token){
+            xhr.setRequestHeader("token", token);
+        }
+    }
+
+    if (method == "POST") { //post
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(data);
+    } else {
+        xhr.send(null); //get 不能发送数据
+    }
+
+    //指定响应处理函数
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            //成功之后调用回调函数
+            if (type == "xml") {
+                //   return callBack(JSON.parse(xhr.responseXML), xhr);
+                if (!xhr.responseXML || typeof xhr.responseXML != "string") {
+                    return callBack(xhr.responseXML, xhr);
+                }
+                if (typeof responseXML == "string") {
+                    return callBack(JSON.parse(xhr.responseXML), xhr);
+                }
+
+            } else if (type == "text") {
+                if (!xhr.responseText || typeof xhr.responseText != "string") {
+                    return callBack(xhr.responseText, xhr);
+                }
+                if (typeof responseText == "string") {
+                    return callBack(JSON.parse(xhr.responseText), xhr);
+                }
+                //   return callBack(xhr.responseText, xhr);
+                //   return callBack(xhr.responseText, xhr);
             }
-            if (typeof responseText == "string") {
-                return callBack(JSON.parse(xhr.responseText), xhr);
-            }
-            //   return callBack(xhr.responseText, xhr);
-            //   return callBack(xhr.responseText, xhr);
-          }
-      }
-  };
+        }
+    };
 
 }
--- public/static/servers/api.js
+++ public/static/servers/api.js
@@ -4,24 +4,24 @@ import { ajax } from './ajax.js';
 const baseUrl = 'http://live.qvbilam.xin:9503'
 //live
 // live/login
-export const login = (data, callBack) => ajax(baseUrl + '/index/login/index', 'POST', true, data, callBack)
-export const sendCode = (data, callBack) => ajax(baseUrl + '/index/send/index', 'POST', true, data, callBack)
+export const login = (data, callBack) => ajax(baseUrl + '/index/login/index', 'POST', true, data, callBack, false)
+export const sendCode = (data, callBack) => ajax(baseUrl + '/index/send/index', 'POST', true, data, callBack, true)
 //live/js/VerToken
-export const send = (callBack) => ajax(baseUrl + '/index/send/index', 'POST', true, null, callBack, 'json')
+export const send = (callBack) => ajax(baseUrl + '/index/send/index', 'POST', true, null, callBack, true, 'json')
 //bank/js/user_chat_push 
-export const chat = (data, callBack) => ajax(baseUrl + '/index/chat/index', 'POST', true, data, callBack)
+export const chat = (data, callBack) => ajax(baseUrl + '/index/chat/index', 'POST', true, data, callBack, true)
 //live/detail
-export const game = (callBack) => ajax(baseUrl + '/index/game/getdata', 'POST', false, null, callBack)
+export const game = (callBack) => ajax(baseUrl + '/index/game/getdata', 'POST', false, null, callBack, true)
 //live/perfect
-export const editUser = (data, callBack) => ajax(baseUrl + '/index/login/perfect', 'POST', true, data, callBack, 'json')
+export const editUser = (data, callBack) => ajax(baseUrl + '/index/login/perfect', 'POST', true, data, callBack, true, 'json')
 
 //bank
 //bank/index
-export const bankBanner = (callBack) => ajax(baseUrl + '/index/bank/banner', 'POST', true, null, callBack)
-export const bankIndex = (callBack) => ajax(baseUrl + '/index/bank/index', 'POST', true, null, callBack)
+export const bankBanner = (callBack) => ajax(baseUrl + '/index/bank/banner', 'POST', true, null, callBack, true)
+export const bankIndex = (callBack) => ajax(baseUrl + '/index/bank/index', 'POST', true, null, callBack, true)
 //bank/detail
-export const detail = (data, callBack) => ajax(baseUrl + '/index/bank/detail?' + data, 'GET', true, null, callBack)
+export const detail = (data, callBack) => ajax(baseUrl + '/index/bank/detail?' + data, 'GET', true, null, callBack, true)
 //bank/fight
-export const fight = (data, callBack) => ajax(baseUrl + '/index/bank/fight?' + data, 'GET', true, null, callBack)
+export const fight = (data, callBack) => ajax(baseUrl + '/index/bank/fight?' + data, 'GET', true, null, callBack, true)
 
 
