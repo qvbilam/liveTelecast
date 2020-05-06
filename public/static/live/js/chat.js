92e12ba6d6b6cb6157feb136f4186ae276c2a87a
zhaocaixia <342892988@qq.com>
2020-05-06 16:31:29 +0800

    'menu'

--- public/static/live/js/chat.js
+++ public/static/live/js/chat.js
@@ -59,9 +59,9 @@ function chatPush(data) {
     var content = $('.comment')
     console.log(content)
     content.oncontextmenu = function (e) {
-        window.onclick = function (e) {
-            document.querySelector('#menu').style.height = 0;
-        }
+        document.oncontextmenu = function () {
+            return false;
+        };
         console.log(e, '!!!!!!!!')
         // var menu = '<div id="menu"><span class="username">' + $("#username").text()
         // menu += '</span><span class="menu">去TA的个人空间</span>'
@@ -77,7 +77,9 @@ function chatPush(data) {
 
         menu.style.display = 'block';
     }
-    
+    window.onclick = function (e) {
+        document.querySelector('#menu').style.height = 0;
+    }
 
 
 }
